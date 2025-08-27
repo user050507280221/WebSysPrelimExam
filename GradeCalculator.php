b3221e944fa4c7b92bbde12fca54a75ff320bce6
user050507280221 <angelbenitezayado477@gmail.com>
2025-08-27 14:59:32 +0000

    Benitez, Prelim Lab Exam

--- GradeCalculator.php
+++ GradeCalculator.php
@@ -1,18 +1,21 @@
 <?php
- if ($_POST) {
-    $qu1z1 = $_POST['qu1z1'];
-    $ass1gn1 = $_POST['ass1gn1'];
-    $ex4m1 = $_POST['ex4m1'];
-    $result = 0;
-    $error = "";
+
+if ($_POST) {
+    $qu1z1 = $_POST['qu1z1']; // Score sa quiz
+    $ass1gn1 = $_POST['ass1gn1']; // Score sa assignment
+    $ex4m1 = $_POST['ex4m1']; // Score sa exam
     
+
+     // Kailngan nng number at mamimili lang sa 0 - 100  numbers and ichecheck if valid ba ang ilalagay na inputs
     if (is_numeric($qu1z1) && is_numeric($ass1gn1) && is_numeric($ex4m1) &&
         $qu1z1 >= 0 && $qu1z1 <= 100 &&
         $ass1gn1 >= 0 && $ass1gn1 <= 100 &&
         $ex4m1 >= 0 && $ex4m1 <= 100) {
-        
+       
+    // ICcompute yung final grade gamit yung weights
          $result = ($qu1z1 * 0.3) + ($ass1gn1 * 0.3) + ($ex4m1 * 0.4);
-    if ($result >= 90) {
+    // Malalaman if anong kalalabasan ng grde.. letter grade
+         if ($result >= 90) {
             $letter = "A";
         } elseif ($result >= 80) {
             $letter = "B";
@@ -25,11 +28,12 @@
         }
     
         
+// ipapakita na sa screen ung final grde and ung letter after macompute
+echo "<div class='result'>Final Grade: " . number_format($result, 1) . "<br> Letter Grade: $letter</div>";
+} else { 
 
-    echo "<p class='result'>Final Grade: " . round($result, 2) . " (Letter Grade: $letter)</p>";
-
-    } else {
-        echo "<p class='result' style='color:red;'>Invalid input. Please enter numbers between 0 and 100.</p>";
+//kung mali yung input na nilagay, pag ma letter na nilagay or negative, sasabihin na mali yung input
+echo "<div class='result' style='color:red;'>Invalid input. Please enter numbers between 0 and 100.</div>";
     }
 }
 ?>
