cd7aa419e1765aa609f3c680dbcf96ec67bb5c26
user050507280221 <angelbenitezayado477@gmail.com>
2025-08-27 10:24:47 +0000

    Benitez, Prelim Lab Exam

--- GradeCalculator.php
+++ GradeCalculator.php
@@ -1,36 +1,35 @@
 <?php
  if ($_POST) {
-    $quiz = $_POST['quiz'];
-    $assign = $_POST['asssign'];
-    $exam = $_POST['exam'];
-    $operation = $_POST['operation'];
+    $qu1z1 = $_POST['qu1z1'];
+    $ass1gn1 = $_POST['ass1gn1'];
+    $ex4m1 = $_POST['ex4m1'];
     $result = 0;
     $error = "";
     
-
-    switch ($operation) {
-        case '+':
-            $result = ($quiz * 0.3) + ($assign * 0.3) + ($exam * 0.4);
-            break;
-        case '-':
-            $result = ($quiz * 0.3) - ($assign * 0.3) - ($exam * 0.4);
-            break;
-        case '*':
-            $result = ($quiz * 0.3) * ($assign * 0.3) * ($exam * 0.4);
-            break;
-        case '/':
-            if ($assign != 0) {
-                $result = $quiz / $exam;
-            } else {
-                $error = "Cannot divide by zero!";
-            }
-            break;
-    }
+    if (is_numeric($qu1z1) && is_numeric($ass1gn1) && is_numeric($ex4m1) &&
+        $qu1z1 >= 0 && $qu1z1 <= 100 &&
+        $ass1gn1 >= 0 && $ass1gn1 <= 100 &&
+        $ex4m1 >= 0 && $ex4m1 <= 100) {
+        
+         $result = ($qu1z1 * 0.3) + ($ass1gn1 * 0.3) + ($ex4m1 * 0.4);
+    if ($result >= 90) {
+            $letter = "A";
+        } elseif ($result >= 80) {
+            $letter = "B";
+        } elseif ($result >= 70) {
+            $letter = "C";
+        } elseif ($result >= 60) {
+            $letter = "D";
+        } else {
+            $letter = "F";
+        }
     
-    if ($error){
-        echo "<p class='result' style='color: red;'>$error</p>";
+        
+
+    echo "<p class='result'>Final Grade: " . round($result, 2) . " (Letter Grade: $letter)</p>";
+
     } else {
-        echo "<p class='result'>Result: $quiz $operation $assign $exam = $result</p>";
+        echo "<p class='result' style='color:red;'>Invalid input. Please enter numbers between 0 and 100.</p>";
     }
 }
- ?>
+?>
